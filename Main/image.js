6be9297eb54f46179671c631b2d03f31667a57e9
Everians <123707023+Everians@users.noreply.github.com>
2025-05-15 14:06:30 +0800

    Add files via upload

---
+++ Main/image.js
@@ -0,0 +1,12 @@
+// // Select the menu button, sidebar, and image container
+// const menuBtn = document.querySelector('.menu-btn');
+// const sidebar = document.querySelector('.sidebar');
+// const imageC = document.querySelector('.imageC');
+
+// // Add event listener to the menu button
+// menuBtn.addEventListener('click', () => {
+//     // Toggle the active class on the sidebar
+//     sidebar.classList.toggle('active');
+//     // Toggle the active class on the image container to apply the movement effect
+//     imageC.classList.toggle('active');
+// });
---
+++ Main/register.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCXOSkOspLYuNgi9SVHaqjpywIJsgJJ68",
+  authDomain: "fand-c2b53.firebaseapp.com",
+  databaseURL: "https://fand-c2b53-default-rtdb.firebaseio.com",
+  projectId: "fand-c2b53",
+  storageBucket: "fand-c2b53.firebasestorage.app",
+  messagingSenderId: "21223514589",
+  appId: "1:21223514589:web:bbf9b9ed99be4d5eb11634",
+  measurementId: "G-JY2C15Q1HM"
+};
